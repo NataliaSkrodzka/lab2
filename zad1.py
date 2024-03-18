55522f96331823b59016a3235f63b82bdd663b78
NataliaSkrodzka <161207800+NataliaSkrodzka@users.noreply.github.com>
2024-03-18 17:37:30 +0100

    lab2

---
+++ zad1.py
@@ -0,0 +1,8 @@
+ulubione_sporty = ['taekwondo', 'hokej', 'koszykowka', 'siatkowka', 'pilka reczna']
+print(ulubione_sporty)
+ulubione_sporty.reverse()
+print(ulubione_sporty)
+
+ulubione_sporty.extend(('lyzwiarstwo', 'plywanie'))
+print(ulubione_sporty)
+
---
+++ zad2.py
@@ -0,0 +1,2 @@
+slownik_skrotow = {'itp.': 'i tym podobne', 'tzw.': 'tak zwany', 'w': 'wedlug', 'ok.': 'okolo'}
+print(slownik_skrotow)
